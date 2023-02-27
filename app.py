2679d2e24a6907226df156c8d84e1934d32d88a3
Ace-9136 <sahilbagate8108@gmail.com>
2023-02-27 17:57:08 +0530

    new navbar

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import csv
 import talib
 import yfinance as yf
 import pandas
-from flask import Flask, escape, request, render_template
+from flask import Flask, escape, request, render_template, redirect, url_for
 from patterns import candlestick_patterns
+from companies import company_name
 import datetime
 
 app = Flask(__name__)
@@ -12,6 +13,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/screener')
+def screener():
     pattern = request.args.get('pattern', False)
     if pattern:
         print(pattern)
@@ -41,11 +47,18 @@ def index():
             except Exception as e:
                 print('failed on filename: ', filename)
 
-    return render_template('dashboard.html', candlestick_patterns=candlestick_patterns, stocks=stocks, pattern=pattern)
+    return render_template('screener.html', candlestick_patterns=candlestick_patterns, stocks=stocks, pattern=pattern)
 
 
-@app.route('/snapshot')
-def snapshot():
+@app.route('/refresh')
+def refresh():
+    return render_template('refresh.html')
+
+
+@app.route('/download_Data', methods=['POST'])
+def download_Data():
+    # Download the dataset
+    """
     with open('datasets/symbols.csv') as f:
         for line in f:
             if "," not in line:
@@ -54,10 +67,10 @@ def snapshot():
             data = yf.download(symbol, start="2022-01-01",
                                end=datetime.date.today())
             data.to_csv('datasets/daily/{}.csv'.format(symbol))
+    """
 
-    return {
-        "code": "success"
-    }
+    print("\nEXECUTION COMPLETED")
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,13 +1,1 @@
-const links = document.querySelectorAll(".sidebar a");
-const sections = document.querySelectorAll(".main section");
 
-links.forEach((link) => {
-  link.addEventListener("click", (event) => {
-    event.preventDefault();
-    const target = event.target.getAttribute("href");
-    sections.forEach((section) => {
-      section.style.display = "none";
-    });
-    document.querySelector(target).style.display = "block";
-  });
-});
